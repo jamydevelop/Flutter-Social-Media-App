27ae269721c6237beec20bb09f4a1155e6221c51
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2024-12-18 12:08:25 +0800

    added firebase dart file

---
+++ social_media_app/lib/features/auth/domain/repository/auth_repo.dart
@@ -0,0 +1,13 @@
+/*
+  Auth Repository - Outlines the possible auth operations for this app.
+ */
+
+import 'package:social_media_app/features/auth/domain/entities/app_user.dart';
+
+abstract class AuthRepo {
+  Future<AppUser?> loginWithEmailPassword(String email, String password);
+  Future<AppUser?> registerWithEmailPassword(
+      String name, String email, String password);
+  Future<void> logout();
+  Future<AppUser?> getCurrentUser();
+}
