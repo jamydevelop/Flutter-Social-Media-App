3d57afbe8111fc57b101db5beeff99c781137ea1
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2024-12-21 21:44:19 +0800

    Update auth_cubit.dart

--- social_media_app/lib/features/auth/presentation/cubits/auth_cubit.dart
+++ social_media_app/lib/features/auth/presentation/cubits/auth_cubit.dart
@@ -1,3 +1,15 @@
 /*
-  Auth Cubit
+  Auth Cubit: State Management
  */
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:social_media_app/features/auth/domain/entities/app_user.dart';
+import 'package:social_media_app/features/auth/domain/repository/auth_repo.dart';
+import 'package:social_media_app/features/auth/presentation/cubits/auth_states.dart';
+
+class AuthCubit extends Cubit<AuthStates> {
+  final AuthRepo authRepo;
+  AppUser? _currentUser;
+
+  AuthCubit({required this.authRepo}) : super(AuthInitial());
+}
