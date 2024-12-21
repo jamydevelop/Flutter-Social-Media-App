f70257bcd45e56c76c3dd71cb87dcdf099341488
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2024-12-21 15:42:12 +0800

    added firebase on project and update firebase_auth_repo

--- social_media_app/lib/features/auth/data/firebase_auth_repo.dart
+++ social_media_app/lib/features/auth/data/firebase_auth_repo.dart
@@ -1,21 +1,54 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:social_media_app/features/auth/domain/entities/app_user.dart';
 import 'package:social_media_app/features/auth/domain/repository/auth_repo.dart';
 
 class FirebaseAuthRepo implements AuthRepo {
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
   @override
-  Future<AppUser?> loginWithEmailPassword(String email, String password) {
-    throw UnimplementedError();
+  Future<AppUser?> loginWithEmailPassword(String email, String password) async {
+    try {
+      // Attempt Sign In
+      UserCredential userCredential = await firebaseAuth
+          .signInWithEmailAndPassword(email: email, password: password);
+
+      //Create User
+      AppUser user = AppUser(
+        uid: userCredential.user!.uid,
+        email: email,
+        name: "",
+      );
+
+      return user;
+    } catch (e) {
+      throw Exception("Login failed $e");
+    }
   }
 
   @override
   Future<AppUser?> registerWithEmailPassword(
-      String name, String email, String password) {
-    throw UnimplementedError();
+      String name, String email, String password) async {
+    try {
+      // Attempt Sign Up
+      UserCredential userCredential = await firebaseAuth
+          .createUserWithEmailAndPassword(email: email, password: password);
+
+      //Create User
+      AppUser user = AppUser(
+        uid: userCredential.user!.uid,
+        email: email,
+        name: name,
+      );
+
+      return user;
+    } catch (e) {
+      throw Exception("Login failed $e");
+    }
   }
 
   @override
-  Future<void> logout() {
-    throw UnimplementedError();
+  Future<void> logout() async {
+    await firebaseAuth.signOut();
   }
 
   @override
